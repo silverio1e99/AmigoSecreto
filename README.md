c5f28cc2668fb07bf5ab0d142b5538bbf34958f2
Sylvester <silverio1e99@gmail.com>
2025-03-15 03:50:59 -0300

    Atualização arquivo app e readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# AmigoSecredto
+# AmigoSecreto
+<h1 align="center"> Desafio: Sorteio de Amigo Secreto </h1>
+
+<div align="center" style="display: flex; align-items: center; justify-content: center;">
+    <img width="50%" src="assets/amigoSecreto.jpg" alt="Imagem ilustrativa do Amigo Secreto">
+</div>
+
+<p>
+    O objetivo desse Challenge era desenvolver um aplicação em Javascript para cadastrar uma lista de pessoas e sortear o Amigo Secreto.
+</p>
+
+## Sobre o desafio
+### Descrição
+
+> O usuário deverá entrar com os nomes um por um
+> Haverá uma validação para que não deixe o nome em branco
+> Cada novo nome cadastrado é atualizado na lista
+> Há um botão para sortear o Amigo Oculto
+
+
+## ✔️ Técnicas e tecnologias utilizadas
+
+- ``Java 8``
+- ``InteliJ IDEA``
+- ``Paradigma de orientação a objetos``
+
+
+<h3 align="center"> 
+    Projeto em construção
+</h3>
--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//Iniciando variáveis
+let amigosInserir = [];
+let lista = document.getElementById('listaAmigos'); //listaAmigos: Nomes de amigos inseridos
+
+//Função para Limpar o campo
+function limparCampo(){
+    nome = document.querySelector('input');
+    nome.value = '';
+}
+
+//Função para atualizar a lista
+function atualizarLista(){
+    lista.innerHTML = '';
+
+    amigosInserir.forEach(amigo => { //amigo: nome do botão
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    })
+}
+
+//Inserir amigos
+function adicionarAmigo(){ //função ao clicar o botão Adicionar
+    let nome = document.querySelector('input').value;
+
+    if (nome == ''){
+        alert("Você não inseriu nenhum nome!");
+        return;
+   } else {
+        amigosInserir.push(nome);
+        limparCampo();
+   }
+   atualizarLista();
+   console.log('Nome registrado: ', nome);
+   return nome;
+}
+
+//Sortear Amigo Secreto
+function sortearAmigo(){
+    if (amigosInserir.length === 0) {
+        alert('Adicione pelo menos um amigo para realizar o sorteio');
+        return;
+    }
+    let amigoSorteado = amigosInserir[Math.floor(Math.random() * amigosInserir.length)];
+    resultado.innerHTML = `<li>O amigo secreto é: ${amigoSorteado}</li>`;
+}
+
+//Função para limpar a lista e recomeçar o sorteio
+function limparLista(){
+    amigosInserir = [];
+    atualizarLista();
+    resultado.innerHTML = '';
+}
